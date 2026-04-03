b1fe7000ac946fac1a31aea7a277baff74de398d
Manolo <manolovp94@gmail.com>
2026-04-03 13:07:51 -0600

    feat(user): List all users

---
+++ src/interfaces/user/user.d.ts
@@ -0,0 +1,32 @@
+export interface RoleDTO {
+    idRole: number;
+    name: string;
+
+}
+
+export interface UserDTO {
+    idUser: number;
+    name: string;
+    middleName?: string;
+    firstLastName: string;
+    secondLastName: string;
+    phone: string;
+    email: string;
+    active: boolean;
+    isFirstLogin: boolean;
+    twoFactorEnabled: boolean;
+    role: RoleDTO;
+    crearedAt: Date;
+}
+
+export interface UserRequestDTO {
+    name: string;
+    middleName?: string;
+    firstLastName: string;
+    secondLastName: string;
+    phone: string;
+    email: string;
+    password: string;
+    active: boolean;
+    roleId: number;
+}
